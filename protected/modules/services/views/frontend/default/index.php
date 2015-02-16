51fc31b82bd4cfdc8e6ed35994ed43811b731d70
kirdyanov <sergik2108@gmail.com>
2015-02-16 12:08:34 +0300

    catalog fix layout

--- protected/modules/services/views/frontend/default/index.php
+++ protected/modules/services/views/frontend/default/index.php
@@ -1,3 +1,5 @@
+<?php $this->beginContent('//layouts/cap'); ?>
 <h1><?php echo $this->title; ?></h1>
 <?php echo $this->catalog_config->text; ?>
 <?php $this->widget('application.modules.services.widgets.CatalogCatToMainWidget'); ?>
+<?php $this->endContent(); ?>
--- protected/modules/services/widgets/views/servicetomain.php
+++ protected/modules/services/widgets/views/servicetomain.php
@@ -1,6 +1,6 @@
 <ul class="objects">
     <?php $this->widget('zii.widgets.CListView', array(
-        'id'=>'product-list',
+        'id'=>'service-list',
         'dataProvider'=>$dataProvider,
         'template'=>"{items}<div class='cl'></div>{pager}",
         'itemView'=>'_serviceView',
