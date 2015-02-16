38c8d3fd399253bd3696e00a7c59f55addef5ef2
kirdyanov <sergik2108@gmail.com>
2015-02-16 12:13:40 +0300

    catalog fix layout

--- protected/modules/services/views/frontend/default/index.php
+++ protected/modules/services/views/frontend/default/index.php
@@ -1,5 +1,3 @@
-<?php $this->beginContent('//layouts/cap'); ?>
 <h1><?php echo $this->title; ?></h1>
 <?php echo $this->catalog_config->text; ?>
 <?php $this->widget('application.modules.services.widgets.CatalogCatToMainWidget'); ?>
-<?php $this->endContent(); ?>
