f35e03193192ca961a2c6dabf4f835f7fc94e726
Pavel Khramkin <xpasha13x@gmail.com>
2015-04-06 15:18:23 +0300

    cms36 done

--- protected/views/frontend/layouts/first_page.php
+++ protected/views/frontend/layouts/first_page.php
@@ -1,28 +1,63 @@
+<?php
+Yii::app()->clientScript->registerScript('show_header_scroll', "
+    $(window).scroll(function() {
+	    if ($(this).scrollTop() > 240){
+	        $('header').hide();
+	        $('.header-scroll').animate({'top': '0px'},0);
+	    }
+	    else{
+	        $('header').show();
+	        $('.header-scroll').stop(true).animate({'top': '-119px'},0);
+	    }
+	});
+", CClientScript::POS_READY);
+?>
+
 <?php $this->beginContent('//layouts/main_layout'); ?>
-<div class="inner">
-    <div class="advs">
-        <?php $this->widget('application.widgets.OutAreaWidget', array('name' => 'preimuschestva-na-glavnoj')); ?>
+    <div class="btn-login-line">
+        <span>Протестировать панель администратора</span><a href="/manage">войти</a>
     </div>
-</div>
-<div class="test-access">
-    <div class="cbg"></div>
-    <div class="inner">
-        <div class="info">
-            <?php $this->widget('application.widgets.OutAreaWidget', array('name' => 'dostupy-na-glavnoj')); ?>
-            <div class="info-lnb"></div>
-            <div class="info-lnt"></div>
+
+    <div class="header-content">
+        <img src="images/fish.png" alt="" class="fish">
+        <?php $this->widget('application.widgets.OutAreaWidget', array('name' => 'zagolovok-v-shapke')); ?>
+        <div class="phone">
+            <?php $this->widget('application.widgets.OutAreaWidget', array('name' => 'telefony-v-shapke')); ?>
         </div>
-        <a class="btn" href="/manage">Войти</a>
     </div>
-</div>
-<div class="inner">
-    <div class="services">
-        <h1>Наши услуги</h1>
-        <?php $this->widget('application.widgets.OutAreaWidget', array('name' => 'uslugi-na-glavnoj')); ?>
+
+    <div class="wave-line"></div>
+
+    <nav>
+        <?php $this->widget('application.widgets.OutMenu', array('name' => 'main')); ?>
+    </nav>
+
+    <div class="about-company">
+        <?php $this->widget('application.widgets.OutAreaWidget', array('name' => 'o-kompanii')); ?>
     </div>
-</div>
-<div class="inner">
-    <?php echo $content; ?>
-    <div class="clear"></div>
-</div>
+    <div class="middle-line">
+        <h2>Ваши услуги</h2>
+
+        <div class="roper-left"></div>
+
+        <?php $this->widget('application.widgets.OutAreaWidget', array('name' => 'vashi-uslugi-na-glavnoj')); ?>
+
+        <div class="roper-right"></div>
+    </div>
+    <div class="advantages-line">
+        <h2>Преимущества</h2>
+        <?php $this->widget('application.widgets.OutAreaWidget', array('name' => 'preimuschestva-na-glavnoj')); ?>
+        <img src="images/boat.png" alt="">
+    </div>
+    <div class="bottom-line">
+        <div class="left-col">© ООО «<span>Fishpangram</span>», 2015</div>
+        <div class="right-col">
+            <?php $this->widget('application.widgets.OutAreaWidget', array('name' => 'sotsseti-v-podvale-shablona')); ?>
+        </div>
+    </div>
+
+    <div class="inner">
+        <?php echo $content; ?>
+        <div class="clear"></div>
+    </div>
 <?php $this->endContent(); ?>
--- protected/views/frontend/layouts/main_layout.php
+++ protected/views/frontend/layouts/main_layout.php
@@ -1,51 +1,90 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<title><?php if ($this->title) echo CHtml::encode($this->title), ' - ', CHtml::encode(Yii::app()->config->sitename);?></title>
-	<meta name="keywords" content="<?php echo CHtml::encode($this->keywords); ?>"/>
-	<meta name="description" content="<?php echo CHtml::encode($this->description); ?>"/>
-	<meta name="language" content="ru" />
-	<meta name="author" content="<?php echo CHtml::encode(Yii::app()->config->author); ?>"/>
-	<link rel="icon" type="image/vnd.microsoft.icon" href="/favicon.ico">
-	<link href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css" rel="stylesheet" type="text/css"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title><?php if ($this->title) echo CHtml::encode($this->title), ' - ', CHtml::encode(Yii::app()->config->sitename); ?></title>
+    <meta name="keywords" content="<?php echo CHtml::encode($this->keywords); ?>"/>
+    <meta name="description" content="<?php echo CHtml::encode($this->description); ?>"/>
+    <meta name="language" content="ru"/>
+    <meta name="author" content="<?php echo CHtml::encode(Yii::app()->config->author); ?>"/>
+    <link rel="icon" type="image/vnd.microsoft.icon" href="/favicon.ico">
+    <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
-	<div class="wrapper">
-		<header>
-			<div class="inner">
-				<div class="top">
-					<a href="/" class="logo"><img src="/images/logo.png" alt=""/></a>
-					<div class="title"><span class="bullet"></span><?php echo CHtml::encode(Yii::app()->config->sitename); ?></div>
-					<div class="phones"><?php $this->widget('application.widgets.OutAreaWidget', array('name' => 'telefony-v-shapke')); ?></div>
-				</div>
-			</div>
-			<div class="banners">
-				<div class="banner" style="background-image: url('/images/banner.jpg');"></div>
-			</div>
-			<div class="inner">
-				<div class="bot">
-					<div class="menu">
-						<?php $this->widget('application.widgets.OutMenu', array('name' => 'main')); ?>
-					</div>
-					<div class="social">
-						<?php $this->widget('application.widgets.OutAreaWidget', array('name' => 'sotsseti-v-shapke')); ?>
-					</div>
-				</div>
-				<div class="line"></div>
-			</div>
-		</header>
-		<div class="content">
-			<?php echo $content; ?>
-		</div>
-		<div class="empty"></div>
-	</div>
-	<footer>
-		<div class="inner">
-			<a href="/" class="logo"><img src="/images/logo_footer.png" alt=""/></a>
-			<div class="title"><span class="bullet"></span><?php echo CHtml::encode(Yii::app()->config->sitename); ?></div>
-			<a href="http://plus1dev.ru/" class="dev"><img src="/images/logo_po.png" alt=""/></a>
-		</div>
-	</footer>
+
+<div class="fond"></div>
+<div class="wrapper">
+
+    <div class="header-scroll inner">
+        <div class="header-line">
+            <a href="" class="back"></a>
+
+            <div class="grid"></div>
+            <a href="" class="logo">сайт-визитка</a>
+
+            <div class="grid"></div>
+            <a href="" class="corp">корпоративный сайт</a>
+            <a href="" class="shop">интернет-магазин</a>
+        </div>
+        <div class="btn-login-line">
+            <span>Протестировать панель администратора</span><a href="/manage">войти</a>
+        </div>
+    </div>
+
+    <header>
+        <div class="top-header">
+            <div class="inner">
+                <div class="left-col">
+                    <a href="#">вернуться на главную</a>
+                </div>
+                <div class="right-col">
+                    <a href="#" class="corp">корпоративный сайт</a>
+                    <a href="#" class="shop">интернет-магазин</a>
+                </div>
+            </div>
+        </div>
+        <div class="middle-header">
+            <div class="logo inner">
+                <a href="#"><?php echo CHtml::encode(Yii::app()->config->sitename); ?><p>демонстрационная версия</p></a>
+
+                <div class="grid"></div>
+            </div>
+        </div>
+    </header>
+
+    <div class="content inner">
+        <?php echo $content; ?>
+    </div>
+
+    <footer>
+        <div class="top-foot ">
+            <div class="inner">
+                <p>
+                    Весь контент на демонстрационной версии является выдуманным, а все совпадения с реальными
+                    организациями - случайны.
+                    <br>
+                    При разработке ни одна рыба не пострадала.
+                </p>
+            </div>
+        </div>
+        <div class="btn-order-line">
+            <div class="inner">
+                <a href="#">заказать</a>
+            </div>
+        </div>
+        <div class="bottom-foot">
+            <div class="inner">
+                <a href="http://plus1dev.ru/" class="left-col">© ООО «Плюс один», 2015</a>
+
+                <div class="right-col">
+                    <a href="#" class="vk"></a>
+                    <a href="#" class="fb"></a>
+                    <a href="#" class="in"></a>
+                    <a href="#" class="tw"></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
 </body>
 </html>
