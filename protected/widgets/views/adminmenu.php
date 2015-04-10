a67bf024b0279c3448206fb26b027b16aa68f1bd
Dmitry Matveev <dimon9107@yandex.ru>
2015-04-10 08:04:45 +0300

    cms38 done

--- protected/widgets/views/adminmenu.php
+++ protected/widgets/views/adminmenu.php
@@ -1,5 +1,6 @@
 <ul>
     <li> <?php echo CHtml::link('Сайт', array('/config')); ?> </li>
     <li> <?php echo CHtml::link('Сменить адрес почты', array('/user/changeemail')); ?> </li>
+	<li> <?php echo CHtml::link('Сменить логин', array('/user/changelogin')); ?> </li>
 	<li> <?php echo CHtml::link('Сменить пароль', array('/user/changepassword')); ?> </li>
 </ul>
