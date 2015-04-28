0d7b5694284ddc2bb1bca3df4739e34d1ae3758f
Eugene Potemin <you@example.com>
2015-04-28 10:14:46 +0300

    cms45_28/1014

--- protected/modules/reviews/widgets/views/reviews.php
+++ protected/modules/reviews/widgets/views/reviews.php
@@ -10,14 +10,17 @@ Yii::app()->clientScript->registerScript('jcarousel', "
 <div class="jcarousel-wrapper">
     <div class="jcarousel">
         <ul>
+            <?php $i = 1;?>
             <?php foreach ($dataProvider as $review):?>
                 <li>
-                    <div class ="feedback-item">
+                    <div class ="feedback-item review<?php echo $i % 3?>">
                         <h2><?php echo $review->name; ?></h2>
                         <p><?php echo $review->text; ?></p>
+                        <p class="feedback-date"><?php echo date("d.m.Y", strtotime($review->date_create)); ?></p>
                         <a href="/reviews"></a>
                     </div>
                 </li>
+                <?php $i++;?>
             <?php endforeach;?>
         </ul>
     </div>
