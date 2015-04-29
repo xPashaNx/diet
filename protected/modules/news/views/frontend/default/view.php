c86689f60faaaae3ecf1f3cecc5966ed7e68128b
Eugene Potemin <you@example.com>
2015-04-29 12:00:03 +0300

    cms45_29/1159

--- protected/modules/news/views/frontend/default/view.php
+++ protected/modules/news/views/frontend/default/view.php
@@ -28,46 +28,33 @@ $cs->registerScript('images', "
 ", CClientScript::POS_READY);
 ?>
 
-<div class="view_item_news">
-    <div class="head_news">
-        <h1><?php echo $titleListNews ?></h1>
+
+    <h1><?php echo $titleListNews ?></h1>
+
+    <div class="news-item">
+        <span><?php echo date("d.m.Y", strtotime($model->date)); ?></span>
+        <h2><?php echo $model->title; ?></h2>
+        <p><?php echo $model->description; ?></p>
     </div>
 
-    <div class="img_news">
-        <?php if ($model->cover_id == 0 && $model->newsImages): ?>
+
+<div class="img_news">
+    <?php if ($model->cover_id == 0 && $model->newsImages): ?>
+        <b>
+            <?php echo CHtml::image('/upload/userfiles/images/' . $model->newsImages[0]->filename, '', array(
+                'style' => 'width:50%; height:50%; float: left; margin-right: 20px;'));?>
+        </b>
+    <?php endif ?>
+
+    <?php foreach ($model->newsImages as $image) : ?>
+        <?php if ($image->id == $model->cover_id) : ?>
             <b>
-                <?php echo CHtml::image('/upload/userfiles/images/' . $model->newsImages[0]->filename, '', array(
+                <?php echo CHtml::image('/upload/userfiles/images/' . $image->filename, '', array(
                     'style' => 'width:50%; height:50%; float: left; margin-right: 20px;'));?>
             </b>
-        <?php endif ?>
-
-        <?php foreach ($model->newsImages as $image) : ?>
-            <?php if ($image->id == $model->cover_id) : ?>
-                <b>
-                    <?php echo CHtml::image('/upload/userfiles/images/' . $image->filename, '', array(
-                        'style' => 'width:50%; height:50%; float: left; margin-right: 20px;'));?>
-                </b>
-            <?php endif; ?>
-        <?php endforeach; ?>
-    </div>
-    <div class="annotation_news">Аннотация: <?= $model->annotation; ?></div>
-    <div class="content_news"><?= $model->description; ?></div>
-
-    <div class="clear"></div>
-
-    <div class="gallery_news">
-        <h3>Фотогалерея:</h3>
-        <?php $this->widget('zii.widgets.CListView', array(
-            'id' => 'gallery-list',
-            'dataProvider' => $imagesDataProvider,
-            'itemView' => '_photoview',
-            'template' => '{items}',
-            'viewData' => array(
-                'folder_upload' => $folder_upload,
-            ),
-        )); ?>
-    </div>
-
-    <hr/>
-    <div class="date_news" style="float: right; font-size: 12px;">Дата создания: <?= $model->date; ?></div>
+        <?php endif; ?>
+    <?php endforeach; ?>
 </div>
+
+<div class="clear"></div>
+
