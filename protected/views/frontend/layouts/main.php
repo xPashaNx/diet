3ff818a1cc90ed4c26411e8aacbb0924e50da209
Eugene Potemin <you@example.com>
2015-04-30 11:22:24 +0300

    fix bugs

--- protected/views/frontend/layouts/main.php
+++ protected/views/frontend/layouts/main.php
@@ -1,14 +1,12 @@
 <?php $this->beginContent('//layouts/main_layout'); ?>
 <div class="content-line">
 	<div class="content-inner">
-		<div class="breadcrumbs">
 		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
 			'homeLink' => CHtml::link('Главная','/'),
-			'separator' => '<span> / </span>',
+			'separator' => '<span class = "breadcrumbs separator"> &#9642; </span>',
 			'links' => $this->breadcrumbs,
 			));
 		?>
-		</div>
 		<?php echo $content; ?>
 		<div class="clear"></div>
 	</div>
