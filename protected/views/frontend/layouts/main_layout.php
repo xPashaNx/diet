dbffed39de6a1e8b07a1e47174c340a633333c5d
Dmitry Matveev <dimon9107@yandex.ru>
2015-05-25 14:28:49 +0300

    layout fix

--- protected/views/frontend/layouts/main_layout.php
+++ protected/views/frontend/layouts/main_layout.php
@@ -1,13 +1,13 @@
 <?php
 Yii::app()->clientScript->registerScript('show_header_scroll', "
     $(window).scroll(function() {
-	    if ($(this).scrollTop() > 293){
-	        $('.header-scroll').animate({'top': '0px'},2);
-	    }
-	    else{
-	        $('.header-scroll').stop(true).animate({'top': '-119px'},2);
-	    }
-	});
+        if ($(this).scrollTop() > 293){
+            $('.header-scroll').animate({'top': '0px'},2);
+        }
+        else{
+            $('.header-scroll').stop(true).animate({'top': '-119px'},2);
+        }
+    });
 ", CClientScript::POS_READY);
 ?>
 
@@ -31,8 +31,8 @@ Yii::app()->clientScript->registerScript('show_header_scroll', "
     <meta name="author" content="<?php echo CHtml::encode(Yii::app()->config->author); ?>"/>
     <link rel="icon" type="image/vnd.microsoft.icon" href="/favicon.ico">
     <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css" rel="stylesheet" type="text/css"/>
-	<script src="/js/lightbox.min.js"></script>
-	<link href="/css/lightbox.css" rel="stylesheet" type="text/css"/>
+    <script src="/js/lightbox.min.js"></script>
+    <link href="/css/lightbox.css" rel="stylesheet" type="text/css"/>
     <link href="/css/responsiveslides.css" rel="stylesheet" type="text/css">
     <link href="/css/jcarousel.responsive.css" rel="stylesheet" type="text/css">
 </head>
@@ -40,22 +40,25 @@ Yii::app()->clientScript->registerScript('show_header_scroll', "
 <div class="wrapper">
     <div class="header-scroll wrapper-inner">
 
-        <a href=".." class="back"></a>
+        <a href="http://plusonecms.ru/" class="back"></a>
         <div class="grid"></div>
-        <a href="" class="logo">сайт-визитка</a>
+        <a href="" class="logo">корпоративный сайт</a>
         <div class="grid"></div>
-        <a href="" class="corp">корпоративный сайт</a>
-        <a href="" class="shop">интернет-магазин</a>
+        <a href="http://viz.plusonecms.ru" class="visitka">сайт-визитка</a>
+        <a href="http://shop.plusonecms.ru" class="shop">интернет-магазин</a>
 
         <div class="btn-login-line">
-            <span>Протестировать панель администратора</span><a href="/manage">войти</a>
+            <a href="<?php echo Yii::app()->createAbsoluteUrl('/manage'); ?>" target="_blank">
+                <span>Протестировать панель администратора</span>
+                <div class="login">войти</div>
+            </a>
         </div>
     </div>
     <header>
         <div class="top-header">
             <div class="wrapper-inner">
                 <div class="left-col">
-                    <a href="http://plusonecms.ru" class="header-icon back-home">вернуться на главную</a>
+                    <a href="http://plusonecms.ru/" class="header-icon back-home">вернуться на главную</a>
                 </div>
                 <div class="right-col">
                     <a href="http://viz.plusonecms.ru" class="header-icon visitka">сайт-визитка</a>
@@ -73,9 +76,13 @@ Yii::app()->clientScript->registerScript('show_header_scroll', "
     <div class="main wrapper-inner">
         <div class="bottom-header-inner">
             <div class="btn-login-line">
-                <span>Протестировать панель администратора</span><a href="/manage">войти</a>
+                <a href="<?php echo Yii::app()->createAbsoluteUrl('/manage'); ?>" target="_blank">
+                    <span>Протестировать панель администратора</span>
+                    <div class="login">войти</div>
+                </a>
             </div>
             <div class="bottom-header">
+                <a href="<?php echo Yii::app()->createAbsoluteUrl('/'); ?>" class="link-shark"></a>
                 <?php $this->widget('application.widgets.OutAreaWidget', array('name' => 'zagolovok-v-shapke')); ?>
                 <div class="phone">
                     <?php $this->widget('application.widgets.OutAreaWidget', array('name' => 'telefony-v-shapke')); ?>
@@ -89,7 +96,7 @@ Yii::app()->clientScript->registerScript('show_header_scroll', "
             <?php echo $content; ?>
         </div>
         <div class="bottom-line">
-            <div class="left-col">© ООО «Sharkcompany», 2015</div>
+            <div class="left-col">© ООО «SHARKPANGRAM», 2015</div>
             <div class="right-col">
                 <?php $this->widget('application.widgets.OutAreaWidget', array('name' => 'sotsseti-v-podvale-shablona')); ?>
             </div>
